36a0422c03c7081f863da71aeb5a93da3ce39a1b
kareemah <ajimobikareemah@gmail.com>
2022-12-04 15:25:23 +0100

    questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,16 @@
 //using selectors inside the element
 // traversing the dom
+
+const questions = [...document.querySelectorAll('.question')];
+
+questions.forEach((question) => {
+  const questionBtn = question.querySelector('.question-btn');
+  questionBtn.addEventListener('click', function () {
+    questions.forEach((item) => {
+      if (question !== item) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  });
+});
