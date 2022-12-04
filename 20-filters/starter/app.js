6c55de6f9ff766de7a638dc6ff868d26f2de8433
kareemah <ajimobikareemah@gmail.com>
2022-12-04 22:30:31 +0100

    display all products

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -1,1 +1,32 @@
-console.log('filters project');
+console.log(products);
+console.log('filter projects');
+
+const productsContainer = document.querySelector('.products-container');
+const inputForm = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+console.log(searchInput);
+
+function fetchProducts(products) {
+  const allProducts = products.map((product) => {
+    const { id, title, company, image, price } = product;
+    return ` <article class="product">
+          <img
+            src="${image}"
+            class="product-img img"
+            alt=""
+          />
+          <footer>
+            <h5 class="product-name">high-back bench</h5>
+            <span class="product-price">$9.99</span>
+          </footer>
+        </article>`;
+  });
+  productsContainer.innerHTML = allProducts.join('');
+}
+
+fetchProducts(products);
+
+inputForm.addEventListener('keyup', function () {
+  const value = searchInput.value();
+  console.log(value);
+});
