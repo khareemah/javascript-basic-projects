3b600837586525931e9920153dd9809e4a9e953e
kareemah <ajimobikareemah@gmail.com>
2022-12-10 14:50:58 +0100

    update

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -9,7 +9,7 @@ const clearBtn = groceryContainer.querySelector('.clear-btn');
 let editFlag = false;
 let editID = null;
 let groceryValue = '';
-const groceryList = [];
+let groceryList = [];
 
 // edit option
 
@@ -25,8 +25,13 @@ groceryForm.addEventListener('submit', function (e) {
     addGroceryItem(groceryValue);
     //update list in local storage
   } else {
+    const itemToEdit = groceryList.find((item) => item.id == editID);
+    itemToEdit.name = grocery.value;
+    console.log(groceryList);
     [...groceryItems.children].forEach((item) => {
-      if (item.dataset.id == editID) {
+      const itemId = item.dataset.id;
+      let itemName = item.querySelector('p').textContent;
+      if (itemId == editID) {
         showMessage({ style: 'success', message: 'grocery item edited' });
         item.querySelector('p').textContent = grocery.value;
         grocery.value = '';
@@ -49,13 +54,13 @@ function showMessage({ style, message }) {
   }, 1000);
 }
 function addGroceryItem(groceryValue) {
-  // render list item
   grocery.value = '';
   const groceryItem = {
     name: groceryValue,
     id: new Date().getTime().toString(),
   };
-  // groceryList.push(groceryItem);
+  groceryList.push(groceryItem);
+  console.log(groceryList);
   const { name, id } = groceryItem;
   const element = document.createElement('article');
   element.setAttribute('data-id', id);
@@ -82,6 +87,8 @@ function addGroceryItem(groceryValue) {
 }
 function deleteItem(e) {
   const target = e.currentTarget.parentElement.parentElement;
+  groceryList = groceryList.filter((item) => item.id !== target.dataset.id);
+  console.log(groceryList);
   groceryItems.removeChild(target);
   showMessage({ style: 'success', message: 'grocery item removed' });
   grocery.value = '';
@@ -101,10 +108,8 @@ function editItem(e) {
   submitBtn.textContent = 'edit';
 }
 
-console.log('hello');
 clearBtn.addEventListener('click', function () {
   const list = groceryItems.children;
-  console.log(list);
 
   groceryContainer.classList.remove('show-container');
   [...list].forEach((item) => {
@@ -113,5 +118,7 @@ clearBtn.addEventListener('click', function () {
   showMessage({ style: 'danger', message: 'grocery list cleared' });
 });
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage() {}
+function removeFromLocalStorage() {}
 // ****** SETUP ITEMS **********
+window.addEventListener('DOMContentLoaded', function () {});
