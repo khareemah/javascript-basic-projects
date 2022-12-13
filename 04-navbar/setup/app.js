5d1cf127b4b635fa9a61d386178d6d970c072ba4
kareemah <ajimobikareemah@gmail.com>
2022-12-13 16:12:04 +0100

    navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,10 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links');
+console.log(linksContainer);
+navToggle.addEventListener('click', function () {
+  linksContainer.classList.toggle('show-links');
+});
