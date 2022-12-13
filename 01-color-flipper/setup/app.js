dd2f24ef3a1ff805e2112a0256bd7fecfbef5b4b
kareemah <ajimobikareemah@gmail.com>
2022-12-13 11:43:15 +0100

    random flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.querySelector('.btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', changeBackground);
+function changeBackground() {
+  const random = getRandomNumber();
+  console.log(random);
+  document.body.style.backgroundColor = colors[random];
+  color.textContent = colors[random];
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,17 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const color = document.querySelector('.color');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function () {
+  let hexColor = '#';
+  for (let i = 0; i < 6; i++) {
+    const random = getRandomNumber();
+    hexColor += hex[random];
+  }
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
