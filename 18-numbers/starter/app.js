e5b83f25ddd2d92867b6c65aaad4198f41d52dab
kareemah <ajimobikareemah@gmail.com>
2022-12-18 21:39:13 +0100

    8-number

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,19 @@
-console.log('numbers project');
+const numbers = [...document.querySelectorAll(".number")];
+function updateCount(number) {
+  const value = parseInt(number.dataset.value);
+  let count = 0;
+  let range = Math.ceil(value / 2000);
+  const timeout = setInterval(function () {
+    count += range;
+    if (count > value) {
+      clearInterval(timeout);
+      number.textContent = `${value}+`;
+    } else {
+      number.textContent = `${count}+`;
+    }
+  }, 1);
+}
+
+numbers.forEach(function (number) {
+  updateCount(number);
+});
