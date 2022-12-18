499c090c3cbb0a70214d38aa838fb77b197f2ad4
kareemah <ajimobikareemah@gmail.com>
2022-12-18 16:38:14 +0100

    dad jokes

--- 21-dad-jokes/final/app.js
+++ 21-dad-jokes/final/app.js
@@ -1,30 +1,30 @@
-const url = 'https://icanhazdadjoke.com/';
+const url = "https://icanhazdadjoke.com/";
 
-const btn = document.querySelector('.btn');
-const result = document.querySelector('.result');
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   fetchDadJoke();
 });
 
 const fetchDadJoke = async () => {
-  result.textContent = 'Loading...';
+  result.textContent = "Loading...";
   try {
     const response = await fetch(url, {
       headers: {
-        Accept: 'application/json',
-        'User-Agent': 'learning app',
+        Accept: "application/json",
+        "User-Agent": "learning app",
       },
     });
     if (!response.ok) {
-      throw new Error(' error');
+      throw new Error(" error");
     }
     const data = await response.json();
 
     result.textContent = data.joke;
   } catch (error) {
     console.log(error.message);
-    result.textContent = 'There was an error...';
+    result.textContent = "There was an error...";
   }
 };
 
--- 21-dad-jokes/starter/app.js
+++ 21-dad-jokes/starter/app.js
@@ -1,1 +1,25 @@
-console.log('Dad Jokes Starter');
+const para = document.querySelector(".result");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", fetchDadJoke);
+
+const url = " https://icanhazdadjoke.com/";
+async function fetchDadJoke() {
+  para.innerText = "loading...";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (response.ok == false) {
+      throw new Error("Page does not exits...");
+    }
+    const data = await response.json();
+    para.innerText = data.joke;
+  } catch (error) {
+    console.log(error);
+    para.textContent = error.message;
+  }
+}
+window.addEventListener("DOMContentLoaded", fetchDadJoke);
