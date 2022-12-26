68e4a60ec208b2a4b96d5cd17b4321d51572d394
kareemah <ajimobikareemah@gmail.com>
2022-12-26 11:17:45 +0100

    fetch and display users

--- 27-pagination/setup/app.js
+++ 27-pagination/setup/app.js
@@ -1,4 +1,10 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+async function start() {
+  const followers = await fetchFollowers();
+  displayFollowers(followers);
+}
+window.addEventListener("DOMContentLoaded", start);
--- 27-pagination/setup/displayFollowers.js
+++ 27-pagination/setup/displayFollowers.js
@@ -1,3 +1,19 @@
-const display = () => {}
+const container = document.querySelector(".container");
+const sectionTitle = document.querySelector(".section-title");
+const display = (followers) => {
+  const followersList = followers.map((follower) => {
+    const { avatar_url, login, html_url } = follower;
+    return `
+       <article class='card'>
+         <img src="${avatar_url}" alt='person' />
+           <h4>${login}</h4>
+         <a href="${html_url}" class="btn">view profile</a>
+       </article>
+       `;
+  });
+  sectionTitle.innerHTML = `<h1>Pagination...</h1>
+      <div class="underline"></div>`;
+  container.innerHTML = followersList.join("");
+};
 
-export default display
+export default display;
