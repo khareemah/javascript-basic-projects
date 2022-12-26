bc102f58dbd760e57f9b69c42c1f8a213a203a2c
kareemah <ajimobikareemah@gmail.com>
2022-12-26 15:31:14 +0100

    fetch and display buttons

--- 27-pagination/setup/app.js
+++ 27-pagination/setup/app.js
@@ -6,13 +6,35 @@ import displayButtons from "./displayButtons.js";
 let activeIndex = 0;
 let pages = [];
 const sectionTitle = document.querySelector(".section-title");
+const btnContainer = document.querySelector(".btn-container");
 
+function setupUI() {
+  displayFollowers(pages[activeIndex]);
+  displayButtons(pages, activeIndex);
+}
 async function start() {
   const followers = await fetchFollowers();
   sectionTitle.innerHTML = `<h1>Pagination...</h1>
       <div class="underline"></div>`;
   pages = paginate(followers);
-  displayFollowers(pages[activeIndex]);
-  displayButtons(pages, activeIndex);
+  setupUI();
 }
+
+btnContainer.addEventListener("click", function (e) {
+  const target = e.target;
+  if (target.classList.contains("page-btn")) {
+    activeIndex = target.dataset.index;
+  } else if (target.classList.contains("prev-btn")) {
+    activeIndex--;
+    if (activeIndex < 0) {
+      activeIndex = pages.length - 1;
+    }
+  } else if (target.classList.contains("next-btn")) {
+    activeIndex++;
+    if (activeIndex == pages.length) {
+      activeIndex = 0;
+    }
+  }
+  setupUI();
+});
 window.addEventListener("DOMContentLoaded", start);
